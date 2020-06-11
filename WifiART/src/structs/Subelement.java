4a2159559f30790a865625f3318c61c0fb0e4cb9
Dylan Evans <dmevans@google.com>
2020-06-11 00:26:13 +0000

    Implemented Generate Tab UI and basic state structure

---
+++ WifiART/src/structs/Subelement.java
@@ -0,0 +1,13 @@
+package structs;
+
+/**
+ * Represents a LCI/LCR subelement
+ */
+public interface Subelement {
+    /**
+     * Produces the hex-buffer encoding for this LCI/LCR subelement
+     *
+     * @return The hex-buffer encoding
+     */
+    String toHexBuffer();
+}
